35ffe25356ab18607be58f449e87331ff600efeb
Jędrzej Nowacki <jedrzej.nowacki@qt.io>
2018-09-12 09:10:47 +0200

    Fix fromatting

--- extensions/extensions.h
+++ extensions/extensions.h
@@ -11,7 +11,7 @@ namespace Extensions
 template<class T>
 struct Ex
 {
-    typedef Ex<T>  Base;
+    typedef Ex<T> Base;
     // Used only for unique address range, allows 8 operations
     // TODO double check if there is no better option then alignment
     Q_DECL_ALIGN(8) static char offset_;
